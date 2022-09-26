ec99f3ff6e258df45782bfe05764f54078ba79c3
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-09-26 20:08:26 +0800

    使用snapkit纯代码布局

---
+++ SnipKitIosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnipKitIosApp
+//
+//  Created by D G on 2022/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
