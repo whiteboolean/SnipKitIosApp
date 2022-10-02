bc09ecdd35c7ee3c6a11af6345913f177796688c
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-10-02 16:33:58 +0800

    纯代码写垂直和水平布局

--- SnipKitIosApp/ViewController.swift
+++ SnipKitIosApp/ViewController.swift
@@ -9,67 +9,87 @@ import UIKit
 import SnapKit
 import TangramKit
 
+/**
+(void)initialize：函数并不会每次创建对象都调用，只有在第一次初始化的时候才会调用，再次创建将不会调用initialize方法。
+ init方法和initCoder方法相似，知识被调用的环境不一样。如果用代码初始化，会调用init方法，从nib文件或者归档(xib、storyboard)进行初始化会调用initCoder。initCoder是NSCoding协议中的方法，NSCoding是负责编码解码，归档处理的协议。
+ loadView：是开始加载view的起始方法，除非手动调用，否则在ViewController的生命周期中只调用一次。
+ viewDidLoad：是我们最常用的方法，类成员对象和变量的初始化我们都会放在这个方法中。在创建类后无论视图展现还是消失，这个方法也只会在布局是调用一次。
+ viewWillAppear:(BOOL)animated：方法 是在视图将要展现出来的时候调用。
+ viewWillLayoutSubviews：方法是在将要布局子视图的时候调用。
+ viewDidLayoutSubviews：方法是在子视图布局完成后调用。
+ viewDidAppear:(BOOL)animated：方法是视图已经出现。
+ viewWillDisappear:(BOOL)animated：方法是视图即将消失。
+ viewDidDisappear:(BOOL)animated：视图已经消失。
+ dealloc：ViewController被释放时调用。
+ */
+
 class ViewController: UIViewController {
+
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        print("loadView")
         
-        view.backgroundColor = .white
+        let rootLayout = TGLinearLayout(.vert)
+        rootLayout.backgroundColor = .white
+        rootLayout.tg_insetsPaddingFromSafeArea = .all
+        self.view = rootLayout
+         
+        let vertTitleLabel = self.createTitleLabel("你好世界")
         
-        let scrollView = UIScrollView()
-        
-    
-        let S = TGLinearLayout(.vert)
-    
-        scrollView.addSubview(S)
+        vertTitleLabel.tg_top.equal(self.topLayoutGuide, offset:10)  //顶部距离前面的视图10
+//        vertTitleLabel.tg_top.equal(10)  //您可以注释上面，解开这句看看运行效果
+        vertTitleLabel.tg_leading.equal(10)
+        rootLayout.addSubview(vertTitleLabel)
         
         
-        S.tg_top.equal(50)
-        S.tg_width.equal(.fill)
-        S.tg_height.equal(.fill)
-        S.tg_vspace = 10
-        S.tg_orientation = .vert
         
-        let A = UILabel()
-        A.text = "A"
-        A.textColor = UIColor(named:"#766868")
-        A.tg_left.equal(20)
-        A.tg_top.equal(30)
-        S.addSubview(A)
         
-        let B = UILabel()
-        B.text = "B"
-        ///文本居中
-        B.textAlignment = .center
-        B.tg_left.equal(20)
-        B.tg_width.equal(40)
-        B.tg_height.equal(40)
-        S.addSubview(B)
         
-        let C = UILabel()
-        C.text = "C"
-        C.tg_right.equal(40)
-        C.tg_width.equal(50)
-        C.tg_height.equal(40)
-        S.addSubview(C)
         
-        let D = UILabel()
-        D.text = "D"
-        D.tg_left.equal(10)
-        D.tg_right.equal(10)
-        D.tg_width.equal(100)
-        D.tg_height.equal(40)
-        S.addSubview(D)
         
-        self.view.addSubview(scrollView)
-        A.backgroundColor = .green
-        B.backgroundColor = .blue
-        C.backgroundColor = .orange
-        D.backgroundColor = .cyan
         
+         
     }
     
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
+
+extension ViewController {
+    
+    func createTitleLabel(_ title :String) ->UILabel{
+        let label = UILabel()
+        label.text = title
+        label.font  = CFTool.font(17)
+        label.sizeToFit()
+        return label
+    }
+    
+    
+    func createContainerLabel(_ title:String, backgroundColor:UIColor) ->UILabel{
+        let label = UILabel()
+        label.text = title
+        label.textAlignment = .center
+        label.textColor = backgroundColor
+        label.font  = CFTool.font(15)
+        label.adjustsFontSizeToFitWidth = true
+        label.layer.shadowColor = CFTool.color(4).cgColor
+        label.layer.shadowRadius = 2
+        label.layer.shadowOpacity = 0.3
+        return label
+    }
+    
+    func createVerticalLinearLayout(){
+        
+        
+    }
 }
 
